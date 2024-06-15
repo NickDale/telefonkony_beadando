14574decf3cec05abdfe6b1216cf5cd1c3bbddfc
Norbert Balogh <norbert.balogh@ewmgroup.com>
2024-06-15 15:15:37 +0200

    WIP

---
+++ Telefonszamok/Telefonszamok/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Telefonszamok
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Telefonszamok/Telefonszamok/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using enTelefonkony;
+using cnTelefonkony;
+
+namespace Telefonszamok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        cnTelefonkony.Model _context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _context = new cnTelefonkony.Model();
+        }
+
+        private void miMindenAdat_Click(object sender, RoutedEventArgs e)
+        {
+            var adatok = _context.enHelysegek.ToList();
+            dgAdatok.ItemsSource = adatok;
+
+        }
+
+        private void miHelyseg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void miExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
